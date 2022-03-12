51030000ffac0e3af738961a017b3ac023756c20
“깃 <hy04730@naver.com>
2022-03-12 18:03:20 +0900

    first commit

--- weather/App.js
+++ weather/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>New React Native App!</Text>
+      <Text>New React Native App!!</Text>
       <StatusBar style="auto" />
     </View>
   );
