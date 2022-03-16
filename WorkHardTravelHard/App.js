1edab1742a34ea5f562e5c16b01a1caced433c5a
“깃 <hy04730@naver.com>
2022-03-16 21:43:47 +0900

    add toDos object

--- WorkHardTravelHard/App.js
+++ WorkHardTravelHard/App.js
@@ -6,9 +6,19 @@ import { theme } from './color';
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === "") return
+    const newToDos = Object.assign({}, toDos, { [Date.now()]: { text, work: working } })
+    setText("");
+    setToDos(newToDos);
+  }
+  console.log(toDos);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -21,8 +31,10 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
         value={text}
+        returnKeyType="done"
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
         style={styles.input}></TextInput>
     </View>
